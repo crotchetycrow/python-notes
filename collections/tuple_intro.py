aea9f80de44166bf44da20e69bce1f5c2aa81197
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-04-08 15:03:39 +0100

    Added tuples dir

---
+++ collections/tuple_intro.py
@@ -0,0 +1,8 @@
+instruments = ("guitar", "piano", "drums", "piano")
+print(instruments, type(instruments))
+
+print(instruments.index("guitar"))
+print(instruments.count("piano"))
+
+# TUPLES ARE IMMUTABLE
+
