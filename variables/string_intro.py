24d9e471590b90ee56c63d67ff97cdb78218af60
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-04-08 12:07:43 +0100

    String interpolation, methods and indexing

--- variables/string_intro.py
+++ variables/string_intro.py
@@ -5,5 +5,46 @@ print(s1 + s2)
 print(f'{s1} {s2}')
 print(f'I wrote this in {s1} and this in {s2}')
 
-# String indexing
+# String Indexing
 
+s = "Hello World!"
+
+c = s[1]
+
+print(c)
+
+# String Slicing
+
+slices = s[6:9]
+
+print(slices+c)
+
+slices2 = s[:8]
+slices3 = s[3:]
+slices4 = s[10:1:-1]
+reverseSlice = s[::-1]
+print(slices2, slices3, slices4)
+print(reverseSlice)
+
+print(len(s))
+
+# String Method
+
+print(s.lower(), s.upper())
+# print(dir(s))
+print(s.replace('e', 'ooo').upper())
+
+a = "here is"
+b = "a string"
+d = "in parts"
+
+e = f'{a} {b} {d}'
+
+print(e.capitalize())
+
+number = 10
+fruits = "apples"
+
+to_print = f'There are {number} {fruits}.'
+
+print(to_print)
