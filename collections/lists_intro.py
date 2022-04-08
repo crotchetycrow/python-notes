b6b0ee233b13daa23be3d49dd27f3d0496ac47d0
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-04-08 14:38:36 +0100

    Added collections dir and played w/ lists

---
+++ collections/lists_intro.py
@@ -0,0 +1,24 @@
+shopping_list = ["food", "food2", "food3", "food4", "food5"]
+
+shopping_list.append("food6")
+print(shopping_list)
+shopping_list.append("food7")
+print(shopping_list)
+shopping_list.remove("food6")
+print(shopping_list)
+
+colours = ["blue", "red", "yellow", "green", "purple"]
+
+h = "hello"
+print(h.upper())
+print(h)
+
+print(colours.append("brown"))
+print(colours)
+
+print("----POP----")
+print(colours.pop())
+print((colours))
+
+mixed = [3, "3", 3.1, True, [1,2,3]]
+print(mixed[-1][-1])
